1b7d94cadc1fcd1ea43a0e45993816494f5894d4
t3llscode <git@t3l.ls>
2025-03-16 15:58:51 +0100

    fix: add missing requirements
    clean up main.py

--- t3_code/main.py
+++ t3_code/main.py
@@ -6,13 +6,15 @@ app = FastAPI(
     version="1.0.0"
 )
 
+
 # Include routers
 from t3_code.router.router_dataset import router as database_router
 
 for r in [database_router]:
-    app.include_router(r, flush=True)
+    app.include_router(r)
 
 
+# Online Status / Health Check
 @app.get("/")
 async def root():
     return {
