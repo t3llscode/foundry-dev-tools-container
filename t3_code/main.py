f518672159ca4c78e67a57c156357c2eca7950b0
t3llscode <git@t3l.ls>
2025-08-14 18:22:59 +0200

    feat: check  for dev / prod
    exact values are  and , defaults to

--- t3_code/main.py
+++ t3_code/main.py
@@ -1,5 +1,6 @@
+import os
 from fastapi import FastAPI
-
+import uvicorn
 
 app = FastAPI(
     title="Foundry DevTools Container",
@@ -8,6 +9,8 @@ app = FastAPI(
     version="1.0.0"
 )
 
+python_env_env = os.getenv("PYTHON_ENV", "production").lower()  # this might be a double-check
+PYTHON_ENV = python_env_env if python_env_env in ["development", "production"] else "production"
 
 # Include routers
 from t3_code.router.router_dataset import router as database_router
@@ -27,5 +30,9 @@ async def root():
 
 if __name__ == "__main__":
     # Run uvicorn using the import string so reload/workers work correctly.
-    import uvicorn
-    uvicorn.run("t3_code.main:app", host="0.0.0.0", port=8000, reload=True, reload_dirs=["/app/t3_code"], timeout_graceful_shutdown=0, timeout_keep_alive=7200, ws_ping_interval=3600, ws_ping_timeout=7200)
+    
+    if PYTHON_ENV == "production":
+        uvicorn.run("t3_code.main:app", host="0.0.0.0", port=8888, timeout_graceful_shutdown=0, timeout_keep_alive=7200, ws_ping_interval=3600, ws_ping_timeout=7200)
+    
+    if PYTHON_ENV == "development":
+        uvicorn.run("t3_code.main:app", host="0.0.0.0", port=8888, reload=True, reload_dirs=["/app/foundry-dev-tools-container/t3_code"], timeout_graceful_shutdown=0, timeout_keep_alive=7200, ws_ping_interval=3600, ws_ping_timeout=7200)
